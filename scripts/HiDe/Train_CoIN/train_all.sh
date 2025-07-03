7fe0b846c1601d284cb55d8e7127f21b4db8f00e
Ghy0501 <100756251+Ghy0501@users.noreply.github.com>
2025-07-03 22:48:01 +0800

    Add files via upload

---
+++ scripts/HiDe/Train_CoIN/train_all.sh
@@ -0,0 +1,17 @@
+pip install -e .
+sh scripts/CoIN/Train_CoIN/Task1.sh
+pip install -e .
+sh scripts/CoIN/Train_CoIN/Task2.sh
+pip install -e .
+sh scripts/CoIN/Train_CoIN/Task3.sh
+pip install -e .
+sh scripts/CoIN/Train_CoIN/Task4.sh
+pip install -e .
+sh scripts/CoIN/Train_CoIN/Task5.sh
+pip install -e .
+sh scripts/CoIN/Train_CoIN/Task6.sh
+pip install -e .
+sh scripts/CoIN/Train_CoIN/Task7.sh
+pip install -e .
+sh scripts/CoIN/Train_CoIN/Task8.sh
+
